f9e798d1c90782f34cfbc070b4efecc5fa8e291c
hayumaro <hayumaro>
2026-05-01 13:19:07 +0900

    Viewのインスタンスを保持して毎回作り直さないようにした。

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,22 +3,25 @@ using System.Windows.Controls;
 
 namespace Mooforest {
     public partial class MainWindow : Window {
+        public Features.IssueManagement.IssueManagementView? ImInstance;
+        public Features.Memo.MemoView? MemoInstance;
+
         public MainWindow() {
             InitializeComponent();
-#if DEBUG
-            Menu.SelectedIndex = 0;
-#endif
         }
 
         private void Menu_SelectionChanged(object sender, SelectionChangedEventArgs args) {
             var tag = ((ListViewItem)Menu.SelectedItem).Tag;
             switch (tag) {
                 case "IssueManagement":
-                    FeatureView.Content = new Features.IssueManagement.IssueManagementView();
+                    ImInstance ??= new Features.IssueManagement.IssueManagementView();
+                    FeatureView.Content = ImInstance;
                     break;
                 case "Schedule":
                     break;
                 case "Memo":
+                    MemoInstance ??= new Features.Memo.MemoView();
+                    FeatureView.Content = MemoInstance;
                     break;
                 default:
                     break;
